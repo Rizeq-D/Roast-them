d3cf9e4158392afcae469790fb83d79c6c2f7c3b
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-06-24 16:37:25 +0200

    created the MainviewModel

--- app/src/main/java/com/example/roastem/Category.kt
+++ app/src/main/java/com/example/roastem/Category.kt
@@ -3,6 +3,6 @@ package com.example.roastem
 data class Category(val idCategory: String, val strCategory: String,
                     val strCategoryThumb: String, val strCategoryDescription: String)
 
-data class Categories(val categories: List<Category>)
+data class CategoriesResponse(val categories: List<Category>)
 
 
